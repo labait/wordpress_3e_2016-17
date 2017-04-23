4af243bfce6df1904f34483fd19a1ae08f570fed
Antonio Molinari <molinari@incode.it>
2017-04-23 17:26:47 +0200

    added js

--- wp-content/themes/understrap-child-startbootstrap-freelancer/functions.php
+++ wp-content/themes/understrap-child-startbootstrap-freelancer/functions.php
@@ -22,4 +22,9 @@ function theme_enqueue_styles() {
   wp_enqueue_style( 'custom-styles', get_stylesheet_directory_uri() . '/css/custom.css', array(), $the_theme->get( 'Version' ) );
 
   wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
+
+  wp_enqueue_script( 'freelancer-scripts', get_stylesheet_directory_uri() . '/js/freelancer.js', array(), $the_theme->get( 'Version' ), true );
+  wp_enqueue_script( 'jqBootstrapValidation-scripts', get_stylesheet_directory_uri() . '/js/jqBootstrapValidation.js', array(), $the_theme->get( 'Version' ), true );
+
+
 }
